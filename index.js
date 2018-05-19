c56da9c4300942ddcaa044372502fdfb044904f1
Alex Holhos <alex.holhos@emanuel.ro>
2018-05-19 11:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function increaseRankBy(n){
  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
  
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + 1).toString()
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
 }
